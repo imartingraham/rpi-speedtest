c03314953598f96f87e72c525c878c6dbd0bc3cf
Ian Graham <imartingraham@gmail.com>
2018-08-21 11:12:03 -0700

    Use Zapier instead of ifttt to allow for more fields

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rpi SpeedTest
-This is a simple script to get data from speedtest.net and send it to IFTTT
+This is a simple script to get data from speedtest.net and send it to Zapier
 
 ## Required packages
 See requirments.txt for versions
@@ -7,7 +7,6 @@ See requirments.txt for versions
 ```
 python-dotenv
 speedtest-cli
-pyfttt
 ```
 ## Setup instructions
 
@@ -18,8 +17,7 @@ Install required packages
 create `.env` file in the rpi-speedtest directory with the following values
 
 ```
-IFTTT_KEY=[your api key from IFTTT]
-IFTTT_EVENT_NAME=[The name of your configured event]
+ZAPIER_ENDPOINT=[your zapier endpoint url]
 ```
 
 ## Run
--- run.py
+++ run.py
@@ -1,23 +1,26 @@
 import os
-import pyfttt
 import subprocess
 import json
-
+import requests
+import datetime
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+# Get WiFi connection
+cmd = "iw dev wlan0 link | grep SSID | awk '{print $2}'"
+wifi_name = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT).communicate()[0]
 # speedtest-cli only runs from the cli so we'll capture the json output
 result, err = subprocess.Popen(["speedtest-cli", "--json"], stdout=subprocess.PIPE).communicate()
 
 if(not err):
   # result will be a list with one item in it. 
   data = json.loads(result.decode('utf-8'))
-  # send data to ifttt
-  pyfttt.send_event(
-    api_key=os.environ.get('IFTTT_KEY'),
-    event=os.environ.get('IFTTT_EVENT_NAME'),
-    value1=data['download'],
-    value2=data['upload'],
-    value3=data['server']['name'] # This is the location of where the test was taken
-  )
+  zapier_data = {
+    'date': str(datetime.datetime.utcnow()),
+    'wifi_name': wifi_name,
+    'location': data['server']['name'],
+    'upload': data['upload'],
+    'download': data['download']
+  }
+  r = requests.post(os.environ.get('ZAPIER_ENDPOINT'), data=zapier_data)
